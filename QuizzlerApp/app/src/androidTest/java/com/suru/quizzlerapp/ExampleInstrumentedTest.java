9c3b1c6db40fa3bb4cd0b6076122663647cb72e3
ItsSuru <swarajdc01@gmail.com>
2020-09-08 22:36:11 +0530

    my app

---
+++ QuizzlerApp/app/src/androidTest/java/com/suru/quizzlerapp/ExampleInstrumentedTest.java
@@ -0,0 +1,21 @@
+package com.suru.quizzlerapp;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
